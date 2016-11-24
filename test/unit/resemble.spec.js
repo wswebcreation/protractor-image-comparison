f5290cf8e757c49db0757b336fcb66d37e651e2d
Wim Selles <w.selles@rn.rabobank.nl>
2016-11-24 10:18:22 +0100

    fix(coverage): optimized tests and coverage to 92.13,80,92.86,92.13 This is starting point

--- test/unit/resemble.spec.js
+++ test/unit/resemble.spec.js
@@ -4,19 +4,14 @@ const fs = require('fs'),
     path = require('path'),
     resemble = require('../../lib/resemble.js');
 
-// const data = { isSameDimensions: true,
-//     dimensionDifference: { width: 0, height: 0 },
-//     misMatchPercentage: '0.00',
-//     analysisTime: 31,
-//     getDiffImage: [Function]
-// };
-
-
 describe('node-resemble.js', () => {
     const peopleImage = path.resolve(__dirname, './example/People.png'),
+        peopleBlackWhiteImage = path.resolve(__dirname, './example/PeopleBW.png'),
         peopleTwoImage = path.resolve(__dirname, './example/People2.png'),
         largeImage = path.resolve(__dirname, './example/LargeImage.png'),
-        smallImage = path.resolve(__dirname, './example/SmallImage.png');
+        smallImage = path.resolve(__dirname, './example/SmallImage.png'),
+        withAntiAliasing = path.resolve(__dirname, './example/withAntiAliasing.png'),
+        withoutAntiAliasing = path.resolve(__dirname, './example/withoutAntiAliasing.png');
 
     describe('analysis', () => {
         it('should retrieve basic analysis on an image', done => {
@@ -28,70 +23,110 @@ describe('node-resemble.js', () => {
     });
 
     describe('compareTo', () => {
-        it('should compare 2 images with each other', done => {
-            resemble(peopleImage).compareTo(peopleImage)
-                .onComplete(data => {
-                    expect(data.misMatchPercentage).toEqual('0.00');
-                    done();
-                });
+        describe('defaults', () => {
+            it('should successful compare 2 identical images with each other', done => {
+                resemble(peopleImage).compareTo(peopleImage)
+                    .onComplete(data => {
+                        expect(data.misMatchPercentage).toEqual('0.00');
+                        done();
+                    });
+            });
+
+            it('should fail comparing 2 identical images with each other with ignoreNothing enabled', done => {
+                resemble(peopleImage).compareTo(peopleImage)
+                    .ignoreNothing()
+                    .onComplete(data => {
+                        expect(data.misMatchPercentage).toEqual('0.00');
+                        done();
+                    });
+            });
+
+            it('should fail comparing 2 non identical images with each other', done => {
+                resemble(peopleImage).compareTo(peopleTwoImage)
+                    .onComplete(data => {
+                        expect(data.misMatchPercentage).toEqual('8.66');
+                        done();
+                    });
+            });
+
+            it('should fail comparing 2 identical images with each other with ignoreNothing enabled', done => {
+                resemble(peopleImage).compareTo(peopleTwoImage)
+                    .ignoreNothing()
+                    .onComplete(data => {
+                        expect(data.misMatchPercentage).toEqual('8.66');
+                        done();
+                    });
+            });
         });
 
-        it('should fail comparing 2 images with each other', done => {
-            resemble(peopleImage).compareTo(peopleTwoImage)
-                .onComplete(data => {
-                    expect(data.misMatchPercentage).toEqual('8.66');
-                    done();
-                });
+        describe('ignoreColors', () => {
+            it('should fail comparing 2 non identical images with each other without ignoreColors enabled', done => {
+                resemble(peopleImage).compareTo(peopleBlackWhiteImage)
+                    .onComplete(data => {
+                        expect(data.misMatchPercentage).toEqual('23.57');
+                        done();
+                    });
+            });
+            it('should succeed comparing 2 non identical images with each other with ignoreColors enabled', done => {
+                resemble(peopleImage).compareTo(peopleBlackWhiteImage)
+                    .ignoreColors()
+                    .onComplete(data => {
+                        expect(data.misMatchPercentage).toEqual('0.00');
+                        done();
+                    });
+            });
         });
 
-        it('should fail comparing 2 images with each other with ignoreNothing enabled', done => {
-            resemble(peopleImage).compareTo(peopleTwoImage)
-                .ignoreNothing()
-                .onComplete(data => {
-                    expect(data.misMatchPercentage).toEqual('8.66');
-                    done();
-                });
+        describe('ignoreAntialiasing', () => {
+            it('should fail comparing 2 non identical images with each other without ignoreAntialiasing enabled', done => {
+                resemble(withAntiAliasing).compareTo(withoutAntiAliasing)
+                    .onComplete(data => {
+                        expect(data.misMatchPercentage).toEqual('0.58');
+                        done();
+                    });
+            });
+
+            it('should succeed comparing 2 non identical images with each other with ignoreAntialiasing enabled', done => {
+                resemble(withAntiAliasing).compareTo(withoutAntiAliasing)
+                    .ignoreAntialiasing()
+                    .onComplete(data => {
+                        expect(data.misMatchPercentage).toEqual('0.00');
+                        done();
+                    });
+            });
         });
 
-        it('should fail comparing 2 images with each other with ignoreAntialiasing enabled', done => {
-            resemble(peopleImage).compareTo(peopleTwoImage)
-                .ignoreAntialiasing()
-                .onComplete(data => {
-                    expect(data.misMatchPercentage).toEqual('5.85');
-                    done();
-                });
-        });
-
-        it('should fail comparing 2 images with each other with ignoreColors enabled', done => {
-            resemble(peopleImage).compareTo(peopleTwoImage)
-                .ignoreColors()
-                .onComplete(data => {
-                    expect(data.misMatchPercentage).toEqual('0.93');
-                    done();
-                });
-        });
-
-        it('should fail comparing 2 images with each other with ignoreRectangles enabled', done => {
-            resemble(peopleImage).compareTo(peopleTwoImage)
-                .ignoreRectangles([{x: 25, y: 75, height: 320, width: 160}])
-                .onComplete(data => {
-                    expect(data.misMatchPercentage).toEqual('4.17');
-                    done();
-                });
-        });
-
-        it('should fail comparing 2 images with not the same dimensions', done => {
-            resemble(largeImage).compareTo(smallImage)
-                .onComplete(data => {
-                    console.log(data)
-                    expect(data.isSameDimensions).toEqual(false);
-                    done();
-                });
+        describe('ignoreRectangles', () => {
+            it('should fail comparing 2 non identical images with each other without ignoreRectangles enabled', done => {
+                resemble(peopleImage).compareTo(peopleTwoImage)
+                    .onComplete(data => {
+                        expect(data.misMatchPercentage).toEqual('8.66');
+                        done();
+                    });
+            });
+            it('should fail comparing 2 non identical images with each other with ignoreRectangles enabled', done => {
+                resemble(peopleImage).compareTo(peopleTwoImage)
+                    .ignoreRectangles([{
+                        x: 15,
+                        y: 15,
+                        height: 200,
+                        width: 460
+                    }, {
+                        x: 0,
+                        y: 200,
+                        height: 300,
+                        width: 460
+                    }])
+                    .onComplete(data => {
+                        expect(data.misMatchPercentage).toEqual('0.00');
+                        done();
+                    });
+            });
         });
     });
 
-    fdescribe('outputSettings', () => {
-        beforeEach(() =>{
+    describe('outputSettings', () => {
+        beforeEach(() => {
             resemble.outputSettings({
                 errorColor: {
                     red: 255,
@@ -105,190 +140,26 @@ describe('node-resemble.js', () => {
             });
         });
 
-        it('should ', done => {
+        it('should fail comparing 2 identical images with not the same dimensions', done => {
+            resemble(largeImage).compareTo(smallImage)
+                .onComplete(data => {
+                    expect(data.isSameDimensions).toEqual(false);
+                    done();
+                });
+        });
+
+        it('should save a difference when 2 non identical images fail comparing', done => {
             resemble(peopleImage)
                 .compareTo(peopleTwoImage)
                 .onComplete(data => {
-                    data.getDiffImage().pack().pipe(fs.createWriteStream(path.resolve(process.cwd(), 'diff.png')));
+                    const filePath = path.resolve(process.cwd(), '.tmp/diff.png');
+
+                    expect(data.misMatchPercentage).toEqual('8.66');
+
+                    data.getDiffImage().pack().pipe(fs.createWriteStream(filePath));
+                    expect(fs.existsSync(filePath)).toBe(true);
                     done();
                 });
         });
     });
-
-    // const EXAMPLE_LARGE_IMAGE = path.resolve(__dirname, './example/LargeImage.png'),
-    //     EXAMPLE_SMALL_IMAGE = path.resolve(__dirname, './example/SmallImage.png'),
-    //     OPTIMISATION_SKIP_STEP = 6,
-    //     DEFAULT_LARGE_IMAGE_THRESHOLD = 1200;
-    // describe('largeImageThreshold', () => {
-    //     describe('when unset', () => {
-    //         describe('when ignoreAntialiasing is enabled', () => {
-    //             it('skips pixels when a dimension is larger than the default threshold (1200)', done => {
-    //                 getLargeImageComparison().ignoreAntialiasing().onComplete(data => {
-    //                     expectPixelsToBeSkipped(data.getDiffImage(), OPTIMISATION_SKIP_STEP);
-    //                     done();
-    //                 });
-    //             });
-    //
-    //             it('does not skip pixels when both dimensions are smaller than the default threshold (1200)', done => {
-    //                 getSmallImageComparison().ignoreAntialiasing().onComplete(data => {
-    //                     expectPixelsNotToBeSkipped(data.getDiffImage(), OPTIMISATION_SKIP_STEP);
-    //                     done();
-    //                 });
-    //             });
-    //         });
-    //
-    //         describe('when ignoreAntialiasing is disabled', () => {
-    //             it('does not skip pixels when a dimension is larger than the default threshold (1200)', done => {
-    //                 getLargeImageComparison().onComplete(data => {
-    //                     expectPixelsNotToBeSkipped(data.getDiffImage(), OPTIMISATION_SKIP_STEP);
-    //                     done();
-    //                 });
-    //             });
-    //
-    //             it('does not skip pixels when both dimensions are smaller than the default threshold (1200)', done => {
-    //                 getSmallImageComparison().onComplete(data => {
-    //                     expectPixelsNotToBeSkipped(data.getDiffImage(), OPTIMISATION_SKIP_STEP);
-    //                     done();
-    //                 });
-    //             });
-    //         });
-    //     });
-    //
-    //     describe('when explicitly set', () => {
-    //         afterAll(() => {
-    //             resemble.outputSettings({largeImageThreshold: DEFAULT_LARGE_IMAGE_THRESHOLD});
-    //         });
-    //
-    //         describe('when ignoreAntialiasing is enabled', () => {
-    //             //this fails
-    //             it('skips pixels on images with a dimension larger than the given threshold', done => {
-    //                 resemble.outputSettings({largeImageThreshold: 999});
-    //                 getSmallImageComparison().ignoreAntialiasing().onComplete(data => {
-    //                     expectPixelsToBeSkipped(data.getDiffImage(), OPTIMISATION_SKIP_STEP);
-    //                     done();
-    //                 });
-    //             });
-    //
-    //             it('does not skip pixels on images with a dimension equal to the given threshold', done => {
-    //                 resemble.outputSettings({largeImageThreshold: 1000});
-    //                 getSmallImageComparison().ignoreAntialiasing().onComplete(data => {
-    //                     expectPixelsNotToBeSkipped(data.getDiffImage(), OPTIMISATION_SKIP_STEP);
-    //                     done();
-    //                 });
-    //             });
-    //
-    //             it('does not skip pixels on images with both dimensions smaller than the given threshold', done => {
-    //                 resemble.outputSettings({largeImageThreshold: 1001});
-    //                 getSmallImageComparison().ignoreAntialiasing().onComplete(data => {
-    //                     expectPixelsNotToBeSkipped(data.getDiffImage(), OPTIMISATION_SKIP_STEP);
-    //                     done();
-    //                 });
-    //             });
-    //         });
-    //
-    //         describe('when ignoreAntialiasing is disabled', () => {
-    //             it('does not skip pixels on images with a dimension larger than the given threshold', done => {
-    //                 resemble.outputSettings({largeImageThreshold: 999});
-    //                 getSmallImageComparison().onComplete(data => {
-    //                     expectPixelsNotToBeSkipped(data.getDiffImage(), OPTIMISATION_SKIP_STEP);
-    //                     done();
-    //                 });
-    //             });
-    //
-    //             it('does not skip pixels on images with a dimension equal to the given threshold', done => {
-    //                 resemble.outputSettings({largeImageThreshold: 1000});
-    //                 getSmallImageComparison().onComplete(data => {
-    //                     expectPixelsNotToBeSkipped(data.getDiffImage(), OPTIMISATION_SKIP_STEP);
-    //                     done();
-    //                 });
-    //             });
-    //
-    //             it('does not skip pixels on images with both dimensions smaller than the given threshold', done => {
-    //                 resemble.outputSettings({largeImageThreshold: 1001});
-    //                 getSmallImageComparison().onComplete(data => {
-    //                     expectPixelsNotToBeSkipped(data.getDiffImage(), OPTIMISATION_SKIP_STEP);
-    //                     done();
-    //                 });
-    //             });
-    //         });
-    //     });
-    //
-    //     describe('when set to a falsy value', () => {
-    //         beforeEach(() => {
-    //             resemble.outputSettings({largeImageThreshold: 0});
-    //         });
-    //
-    //         afterAll(() => {
-    //             resemble.outputSettings({largeImageThreshold: DEFAULT_LARGE_IMAGE_THRESHOLD});
-    //         });
-    //
-    //         describe('when ignoreAntialiasing is enabled', () => {
-    //             it('does not skip pixels on images with a dimension larger than the default threshold (1200)', done => {
-    //                 getLargeImageComparison().ignoreAntialiasing().onComplete(data => {
-    //                     expectPixelsNotToBeSkipped(data.getDiffImage(), OPTIMISATION_SKIP_STEP);
-    //                     done();
-    //                 });
-    //             });
-    //
-    //             it('does not skip pixels on images with a dimension smaller than the default threshold (1200)', done => {
-    //                 getSmallImageComparison().ignoreAntialiasing().onComplete(data => {
-    //                     expectPixelsNotToBeSkipped(data.getDiffImage(), OPTIMISATION_SKIP_STEP);
-    //                     done();
-    //                 });
-    //             });
-    //         });
-    //
-    //         describe('when ignoreAntialiasing is disabled', () => {
-    //             it('does not skip pixels on images with a dimension larger than the default threshold (1200)', done => {
-    //                 getLargeImageComparison().onComplete(data => {
-    //                     expectPixelsNotToBeSkipped(data.getDiffImage(), OPTIMISATION_SKIP_STEP);
-    //                     done();
-    //                 });
-    //             });
-    //
-    //             it('does not skip pixels on images with a dimension smaller than the default threshold (1200)', done => {
-    //                 getSmallImageComparison().onComplete(data => {
-    //                     expectPixelsNotToBeSkipped(data.getDiffImage(), OPTIMISATION_SKIP_STEP);
-    //                     done();
-    //                 });
-    //             });
-    //         });
-    //     });
-    //
-    //     function expectPixelsToBeSkipped(image, step) {
-    //         expect(getPixelForLocation(image, 1, step - 1).alpha).not.toBe(0);
-    //         expect(getPixelForLocation(image, 1, step).alpha).toBe(0);
-    //         expect(getPixelForLocation(image, 1, step + 1).alpha).not.toBe(0);
-    //
-    //         expect(getPixelForLocation(image, step - 1, 1).alpha).not.toBe(0);
-    //         expect(getPixelForLocation(image, step, 1).alpha).toBe(0);
-    //         expect(getPixelForLocation(image, step + 1, 1).alpha).not.toBe(0);
-    //
-    //         expect(getPixelForLocation(image, step, step).alpha).toBe(0);
-    //     }
-    //
-    //     function expectPixelsNotToBeSkipped(image, step) {
-    //         expect(getPixelForLocation(image, 1, step).alpha).not.toBe(0);
-    //         expect(getPixelForLocation(image, step, 1).alpha).not.toBe(0);
-    //         expect(getPixelForLocation(image, step, step).alpha).not.toBe(0);
-    //     }
-    // });
-    //
-    // function getLargeImageComparison() {
-    //     return resemble(EXAMPLE_LARGE_IMAGE).compareTo(EXAMPLE_LARGE_IMAGE);
-    // }
-    //
-    // function getSmallImageComparison() {
-    //     return resemble(EXAMPLE_SMALL_IMAGE).compareTo(EXAMPLE_SMALL_IMAGE);
-    // }
-    //
-    // function getPixelForLocation(image, x, y) {
-    //     var index = (image.width * y + x) << 2;
-    //     return {
-    //         red: image.data[index],
-    //         green: image.data[index + 1],
-    //         blue: image.data[index + 2],
-    //         alpha: image.data[index + 3]
-    //     };
-    // }
 });
