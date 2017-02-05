6c98172c800f1cefa68e7b010a979a7ad0008e59
Wim Selles <w.selles@rn.rabobank.nl>
2017-02-05 18:10:49 +0100

    test(resemblejs): fix jasmine.spec.js by removing the focus

--- test/jasmine.spec.js
+++ test/jasmine.spec.js
@@ -137,7 +137,7 @@ describe('protractor-image-comparison', () => {
         });
 
         if (browser.browserName === 'chrome') {
-            fdescribe('resemble api', () => {
+            describe('resemble api', () => {
                 it('should succeed comparing 2 non identical images with each other with ignoreAntialiasing enabled', () => {
                     browser.executeScript('arguments[0].scrollIntoView(); arguments[0].style.color = "#2d7091";', dangerAlert.getWebElement())
                         .then(() => browser.sleep(500))
