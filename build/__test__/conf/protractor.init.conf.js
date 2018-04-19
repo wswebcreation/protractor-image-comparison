9a4303ca4018848aa4d0e3ae4c3e124d5326cb1d
wimselles <wim.selles@tele2.com>
2018-04-19 10:03:55 +0200

    chore: removed build folder

--- build/__test__/conf/protractor.init.conf.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-let config = require('./protractor.local.conf.js').config;
-config.specs = ['../init.spec.js'];
-exports.config = config;
-//# sourceMappingURL=protractor.init.conf.js.map
--- build/src/browser.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=browser.js.map
